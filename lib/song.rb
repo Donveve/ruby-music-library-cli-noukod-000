3b631a4bca1f220d2055c92de8fd4c88f6f27865
Veniel Dorestave <veniel@noukod.com>
2018-10-19 16:26:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require 'pry'
 class Song
   extend Concerns::Findable
-   attr_accessor :name, :artist, :genre
+  attr_accessor :name
+  attr_reader :artist, :genre
   @@all = []
 
   def initialize(name, artist = nil, genre = nil)
