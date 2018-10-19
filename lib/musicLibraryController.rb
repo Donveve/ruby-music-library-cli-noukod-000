46a281dea0b296e35860fae8261a2028fb298a8f
Veniel Dorestave <veniel@noukod.com>
2018-10-19 16:57:21 +0000

    automatically backed up by learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -116,7 +116,7 @@ end
     puts "Which song number would you like to play?"
   selected_input = gets.strip.to_i
     list_of_songs =  Song.all.sort_by{ |a| a.name }
-    list_of_songs.each.with_index(1) do |song, songs|
+    list_of_songs.each.with_index(1) do |a, songs|
     if song == selected_input
     puts "Playing #{a.name} by #{song.artist.name}"
     end
